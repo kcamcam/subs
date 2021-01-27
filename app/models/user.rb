d1fd5049aab8c683e5e9f00ddfcb402a422cd4ce
Kevin Camellini <kevincamellini@gmail.com>
2021-01-27 00:49:40 -0500

    adds schema info to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+#
+# Schema Info
+#
+# Table name:  users
+#
+# uid         :string:index
+# created_at  :datetime
+# updated_at  :datetime
+#
+
 class User < ApplicationRecord
   validates :uid, presence: true, uniqueness: true
   has_many :subscriptions
