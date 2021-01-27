6b639497669159e406cb7754eb1593a601447341
Kevin Camellini <kevincamellini@gmail.com>
2021-01-27 00:45:32 -0500

    adds enum unit to subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,21 @@
+#
+# Schema Info
+#
+# Table name: subscriptions
+#
+# brand_id       :bigint:index
+# user_id        :bigint:index
+# amount         :decimal
+# frequency      :integer
+# unit           :string
+# created_at     :datetime
+# updated_at     :datetime
+#
 class Subscription < ApplicationRecord
   belongs_to :brand
   belongs_to :user
+  # see ActiveSupport::Duration::PARTS
+  # eval('frequency.unit') to convert '7.days' to 7.days
+  enum unit: { years: 'years', months: 'months', weeks: 'weeks', days: 'days',
+               hours: 'hours', minutes: 'minutes', seconds: 'seconds'}
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,7 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{model.name}"
+    "uploads/#{model.class.to_s.underscore}/#{model.name.downcase}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -50,6 +50,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "#{model.name.underscore}.png" if original_filename
+    "#{model.name.downcase.underscore}.png" if original_filename
   end
 end
