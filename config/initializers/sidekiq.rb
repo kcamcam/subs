d0887c19d0936e5794e11ee6208433bb15a6a822
Kevin Camellini <kevincamellini@gmail.com>
2021-01-31 15:43:46 -0500

    move rails configs to rails credentials

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,8 @@
+
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379', size: 1, network_timeout: 5 }
+  config.redis = { url: Rails.application.credentials[:redis_url], size: 1, network_timeout: 5 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379', size: 20, network_timeout: 5 }
+  config.redis = { url: Rails.application.credentials[:redis_url], size: 20, network_timeout: 5 }
 end
