5cef4ee8d2b2eba21ade5dca0865bc6267130954
Kevin Camellini <kevincamellini@gmail.com>
2021-01-31 13:59:18 -0500

    upadate sidekiq config for dev/prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL'), size: 1, network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379', size: 1, network_timeout: 5 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL'), size: 12, network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379', size: 12, network_timeout: 5 }
 end
