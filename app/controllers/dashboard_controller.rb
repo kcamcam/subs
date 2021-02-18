71e63385892581491436491cbf2429d0aa52f20a
Kevin Camellini <kevincamellini@gmail.com>
2021-02-18 14:29:24 -0500

    adds least and most expensive to dashrboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,8 @@ class DashboardController < ApplicationController
   def index
     @subscriptions = Current.user.subscriptions
     @total = @subscriptions.map { |s| s.amount_in(filter_params[:duration]) }.sum
+    @totals = @subscriptions.map { |s| [s.brand.name.capitalize, s.amount_in(filter_params[:duration])] }
+                            .sort_by { |_key, value| value }
   end
 
   def filter_params
